f8eaffddb7440c86c36aa890394e0e24c68aa9ad
10000JI <sksk436@naver.com>
2024-02-16 00:03:05 +0900

    first commit

---
+++ backend/src/test/java/place/skillexchange/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package place.skillexchange.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
