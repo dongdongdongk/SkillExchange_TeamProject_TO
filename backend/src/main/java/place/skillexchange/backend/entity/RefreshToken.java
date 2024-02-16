c640ec82097d6aa89271ecd265634602c0cb729e
10000JI <sksk436@naver.com>
2024-02-16 17:18:20 +0900

    create entity

---
+++ backend/src/main/java/place/skillexchange/backend/entity/RefreshToken.java
@@ -0,0 +1,31 @@
+package place.skillexchange.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer tokenId;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Date expirationTime;
+
+    @OneToOne
+    private User user;
+}
--- backend/src/main/java/place/skillexchange/backend/entity/User.java
+++ backend/src/main/java/place/skillexchange/backend/entity/User.java
@@ -1,4 +1,100 @@
 package place.skillexchange.backend.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "user_id", length = 50, unique = true)
+    private String id;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @Column(name = "active")
+    @ColumnDefault("0")
+    private boolean active;
+
+    @Column(name = "gender", length = 10)
+    private String gender;
+
+    @Column(name = "job", length = 50)
+    private String job;
+
+    @Column(name = "career_skills", length = 100)
+    private String careerSkills;
+
+    @Column(name = "preferred_subject", length = 50)
+    private String preferredSubject;
+
+    @Column(name = "my_subject", length = 50)
+    private String mySubject;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority : authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getAuthorityName()));
+        }
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
