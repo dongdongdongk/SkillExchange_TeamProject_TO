fad4eac67c6bcbf1c281a052832dad2987c58096
dongdongdongk <dhk9309@gmail.com>
2024-03-03 00:21:53 +0900

    notice 게시판 로직 작성

--- frontend/src/components/Share/TableForm.jsx
+++ frontend/src/components/Share/TableForm.jsx
@@ -1,199 +1,79 @@
-import { useMemo } from 'react';
 import {
   MaterialReactTable,
   useMaterialReactTable,
-} from 'material-react-table';
-
-//nested data is ok, see accessorKeys in ColumnDef below
-const data = [
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
-  },
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
-  },
-];
+} from "material-react-table";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
 
 const TableForm = () => {
-  //should be memoized or stable
+  const [receivedData, setReceivedData] = useState(null);
+
+  // 데이터를 불러오는 함수
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/v1");
+      const data = response.data;
+      setReceivedData(data);
+    } catch (error) {
+      console.error("데이터를 불러오는 중 오류 발생:", error.message);
+    }
+  };
+
+  // 컴포넌트가 마운트된 후에 데이터 불러오기
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // useMemo 내부로 이동
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: "id",
+        header: "글번호",
         size: 150,
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
+        accessorKey: "writer",
+        header: "글쓴이",
         size: 150,
       },
       {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: "title",
+        header: "제목",
         size: 200,
       },
       {
-        accessorKey: 'city',
-        header: 'City',
+        accessorKey: "city",
+        header: "조회수",
         size: 150,
       },
       {
-        accessorKey: 'state',
-        header: 'State',
+        accessorKey: "regdate",
+        header: "등록일",
         size: 150,
       },
     ],
-    [],
+    []
   );
 
+  // useMaterialReactTable 내부로 이동
   const table = useMaterialReactTable({
     columns,
-    data,
+    receivedData,
     mantinePaginationProps: {
       showRowsPerPage: false,
     },
-    paginationDisplayMode: 'pages',
+    paginationDisplayMode: "pages",
     enableColumnFilters: false,
     enableDensityToggle: false,
     enableHiding: false,
     enableSorting: false,
     enableColumnActions: false,
     muiTablePaperProps: {
-      elevation: 0, //change the mui box shadow
-      //customize paper styles
+      elevation: 0,
       sx: {
-        borderRadius: '0',
-        border: '0px',
+        borderRadius: "0",
+        border: "0px",
       },
     },
   });
